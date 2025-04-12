aa530984765b1507263a6c78a721446fb4f18348
sandeep <sandeeppatil10705@gmail.com>
2025-04-12 09:57:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import router from './routes/product.route.js'
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import router from './routes/product.route.js';
 
 dotenv.config();
 
@@ -22,13 +17,6 @@ app.use(cors({
 
 // Middleware
 app.use(express.json());
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-    });
-}
 
 // Routes
 app.use('/api/products', router);
