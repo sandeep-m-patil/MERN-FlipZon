fd05ec7c88d6208c2b28b13bf30ebf04e410ecb1
sandeep <sandeeppatil10705@gmail.com>
2025-04-12 19:17:43 +0530

    restored changes ..

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-const API_URL = 'https://mern-flip-zon.vercel.app/api';
+const API_URL = 'http://localhost:5000/api';
 
 export const useProductStore = create((set) => ({
     products: [],
